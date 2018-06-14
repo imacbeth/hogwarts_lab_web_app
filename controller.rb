d1d3c331a7190e08e895051542846c58cc887db5
Iona Macbeth <ionalmacbeth@gmail.com>
2018-06-14 14:15:11 +0100

    part 5 passed

---
+++ controller.rb
@@ -0,0 +1,23 @@
+require("sinatra")
+require("sinatra/contrib/all")
+require('pry')
+require_relative("./models/students.rb")
+also_reload('./models/*')
+
+#index
+get '/students' do
+  @students = Student.find_all()
+  erb(:index)
+end
+
+#new
+
+get '/students/new' do
+  erb(:new)
+end
+
+#show
+get '/students/:id' do
+  @student1 = Student.new.find_all(params[:id].to_i)
+  erb(:show)
+end
